e3cac9ea02bef3d455a34ae58d1cce36b5b0a3e4
server linux pruebas <onikominetum@gmail.com>
2021-10-06 08:37:42 +0800

    Error settings

--- siteInetum/settings.py
+++ siteInetum/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2@tsr_-y=^0e&5@d^*$r)f@*a^9_2dhleoa_kh5)-oz@-e-i(&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
