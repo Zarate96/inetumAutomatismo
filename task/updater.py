1f0c941480e4870b1d1e1a424f9df4abbfeed924
Your Name <you@example.com>
2022-02-09 10:23:38 -0600

    updated id of hc

--- task/updater.py
+++ task/updater.py
@@ -1,12 +1,17 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
-from task import chat_bot, email, querys
+from task import chat_bot, email, querys, selenium
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(querys.get_migraciones_dalia, 'cron', day_of_week='0-6', hour='10')
+    #scheduler.add_job(querys.get_migraciones_dalia, 'cron', day_of_week='0-6', hour='7')
     scheduler.add_job(querys.get_data_problema_titulo, 'cron', day_of_week='0-4', hour='10')
-    scheduler.add_job(querys.get_ordenes_movil, 'cron', day_of_week='0-6', hour='20')
+    scheduler.add_job(selenium.get_hcAPN, 'cron', day_of_week='0-6', hour='9')
+    scheduler.add_job(selenium.get_hcRutasInter, 'cron', day_of_week='0-6', hour='7')
+    scheduler.add_job(selenium.get_hcRutasInter, 'cron', day_of_week='0-6', hour='15')
+    scheduler.add_job(selenium.get_hcRutasInter, 'cron', day_of_week='0-6', hour='23')
+    #scheduler.add_job(querys.get_migraciones_dalia, 'interval', minutes=60)
+    #scheduler.add_job(querys.get_ordenes_movil, 'cron', day_of_week='0-6', hour='20')
     #scheduler.add_job(querys.get_migraciones_dalia, 'interval', minutes=1)
     #scheduler.add_job(chat_bot.telegram_api, 'interval', minutes=1)
     #scheduler.add_job(email.enviomail, 'interval', minutes=5)
