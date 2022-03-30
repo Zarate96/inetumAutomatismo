460f66fa5355aea95bcd18fa90a58cd0b3045ad2
Your Name <you@example.com>
2022-03-29 22:00:35 -0600

    delete hc and kpi apps

--- cambio/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class CambioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    verbose_name= 'Gestión de Cambio'
-    name = 'cambio'
--- cambio_gics/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CambioGicsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cambio_gics'
-    verbose_name = 'Gestión de Cambio Gics'
